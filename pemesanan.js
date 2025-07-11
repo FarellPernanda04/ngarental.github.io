7fcffed88d11e9a377cd90d826b671eb41067a6e
FarellPernanda04 <farellpernanda@students.amikom.ac.id>
2025-07-11 12:55:33 +0700

    Add files via upload

---
+++ pemesanan.js
@@ -0,0 +1,33 @@
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        event.preventDefault();
+
+        if (!form.checkValidity()) {
+          event.stopPropagation();
+          form.classList.add("was-validated");
+          return;
+        }
+
+        // SweetAlert
+        Swal.fire({
+          title: "Success!",
+          text: "Pesanan anda telah diterima. Check email anda untuk melanjutkan pembayaran.",
+          icon: "success",
+          confirmButtonText: "OK",
+          confirmButtonColor: "#106EBE",
+        }).then(() => {
+          form.reset();
+          form.classList.remove("was-validated");
+        });
+      },
+      false
+    );
+  });
+})();
